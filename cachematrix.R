140a04b14ccd6217e31808b979c576f0000cd0c8
paulhdickinson <dapaulness@yahoo.com>
2014-04-27 03:03:55 -0700

    Completed assignment
    Added code adapted from the example code, which stores and retrieves the
    inverses of square matrices.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Function which takes a matrix as input, and outputs a list of length four, containing functions which store and retrieve the inverse of the matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
+
+	cachedInv <- NULL
+						
+        set <- function(y) {
+
+                x <<- y
+
+                cachedInv <<- NULL
+
+        }
+
+        get <- function() x
+
+        setInv <- function(solve) cachedInv <<- solve
+
+        getInv <- function() cachedInv
+
+        list(set = set, get = get,
+
+             setInv = setInv,
+
+             getInv = getInv)
 
 }
 
 
-## Write a short comment describing this function
+## Function which takes the output list from makeCacheMatrix as input, and computes and stores the inverse if it has not already been computed, or retrieves it and displays a message if it has, and then returns the inverse.
 
 cacheSolve <- function(x, ...) {
+
         ## Return a matrix that is the inverse of 'x'
+
+        cachedInv <- x$getInv()
+
+        if(!is.null(cachedInv)) {
+
+                message("getting cached data")
+
+                return(cachedInv)
+
+        }
+
+        data <- x$get()
+
+        cachedInv <- solve(data, ...)
+
+        x$setInv(cachedInv)
+
+        cachedInv
 }
